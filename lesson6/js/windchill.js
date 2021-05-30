d3e89dd8381bcaa02553d603e453ec1217d57aa5
JulioCaballero63 <juliocl_14@hotmail.com>
2021-05-29 21:47:24 -0600

    update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,3 +1,16 @@
-if (d.getDay() == 5) {
-  document.querySelector("#banner").style.display = "block";
+const t = parseInt(document.querySelector("#temp").textContent);
+const s = parseInt(document.querySelector("#speed").textContent);
+const feelsLike = `${Math.floor(windChill(t, s))} &deg;F`;
+
+if (t <= 50 && s > 3) {
+  document.querySelector("#feels").innerHTML = feelsLike;
+}
+
+function windChill(temp, speed) {
+  let f =
+    35.74 +
+    0.6215 * temp -
+    35.75 * Math.pow(speed, 0.16) +
+    0.4275 * temp * Math.pow(speed, 0.16);
+  return f;
 }

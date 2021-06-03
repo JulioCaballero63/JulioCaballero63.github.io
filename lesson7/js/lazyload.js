9351243ab9a58f6be3021ae29b0cc40bac937d8d
JulioCaballero63 <juliocl_14@hotmail.com>
2021-06-03 15:23:50 -0500

    update

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,11 +1,32 @@
-// let imagesToLoad = document.querySelectorAll('img[data-src]');
-// const loadImages = (image) => {
-//   image.setAttribute('src', image.getAttribute('data-src'));
-//   image.onload = () => {
-//     image.removeAttribute('data-src');
-//   };
-// };
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-// imagesToLoad.forEach((img) => {
-//   loadImages(img);
-// });
+const imageOptions = {
+  threshold: 0.5,
+  rootMargin: "0px 0px 40px 0px",
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imageOptions);
+
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}

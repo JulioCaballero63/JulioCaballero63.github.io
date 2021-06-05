20e11fdf5a7786c8863e2c7289dfd000a4708859
JulioCaballero63 <juliocl_14@hotmail.com>
2021-06-05 03:40:21 -0500

    update

--- lesson6/js/index.js
+++ lesson6/js/index.js
@@ -42,3 +42,30 @@ currentdate.textContent = formateddate;
 if (d.getDay() == 5) {
   document.querySelector("#banner").style.display = "block";
 }
+
+// Days since last visit
+
+const current = [d.getDay(), d.getMonth() + 1, d.getFullYear()].join("/");
+localStorage.setItem("start", "1/1/21");
+localStorage.setItem("end", current);
+
+function getNumberOfDays(start, end) {
+  const date1 = new Date(start);
+  const date2 = new Date(end);
+
+  // One day in milliseconds
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  // Calculating the time difference between two dates
+  const diffInTime = date2.getTime() - date1.getTime();
+
+  // Calculating the no. of days between two dates
+  const diffInDays = Math.round(diffInTime / oneDay);
+
+  return diffInDays;
+}
+
+document.querySelector("#days-since-last-visit").innerHTML =
+  "Days Since Last Visit:" +
+  " " +
+  getNumberOfDays(localStorage.getItem("start"), current);

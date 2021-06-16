98be28ba4b6fea96a610f8080dd823727c192722
JulioCaballero63 <juliocl_14@hotmail.com>
2021-06-15 21:25:51 -0500

    update

---
+++ lesson9/js/index.js
@@ -0,0 +1,44 @@
+// Menu Toggle
+
+const menu = document.querySelector(".hamburger");
+const navigation = document.querySelector(".nav-items");
+
+menu.addEventListener(
+  "click",
+  () => {
+    navigation.classList.toggle("responsive");
+  },
+  false
+);
+
+window.onresize = () => {
+  if (window.innerWidth > 760) navigation.classList.remove("responsive");
+};
+
+const burger = document.querySelector(".button");
+
+burger.addEventListener("click", () => {
+  if (burger.innerText === "❌") {
+    burger.textContent = "🍔 Menu";
+  } else {
+    burger.textContent = "❌";
+  }
+});
+
+// Date JS
+
+const currentdate = document.querySelector("#date");
+
+let d = new Date();
+
+let formateddate = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "full",
+}).format(d);
+
+currentdate.textContent = formateddate;
+
+// Aside
+
+if (d.getDay() == 5) {
+  document.querySelector("#banner").style.display = "block";
+}

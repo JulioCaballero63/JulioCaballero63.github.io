34c8662025d04efd165edc75be098c799fcf84c5
JulioCaballero63 <juliocl_14@hotmail.com>
2021-06-24 21:29:25 -0500

    update

--- lesson10/js/index.js
+++ lesson10/js/index.js
@@ -65,6 +65,24 @@ fetch(apiURL)
     // document.getElementById("icon").setAttribute("alt", desc);
   });
 
+// Windchill
+const t = parseInt(document.querySelector("#temp").textContent);
+const s = parseInt(document.querySelector("#speed").textContent);
+const feelsLike = `${Math.floor(windChill(t, s))} &deg;F`;
+
+if (t <= 50 && s > 3) {
+  document.querySelector("#feels").innerHTML = feelsLike;
+}
+
+function windChill(temp, speed) {
+  let f =
+    35.74 +
+    0.6215 * temp -
+    35.75 * Math.pow(speed, 0.16) +
+    0.4275 * temp * Math.pow(speed, 0.16);
+  return f;
+}
+
 // Web font load
 
 WebFont.load({

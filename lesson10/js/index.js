d0dd724356017fee64d22808df3854c80e503d27
JulioCaballero63 <juliocl_14@hotmail.com>
2021-06-26 07:12:55 -0500

    update

--- lesson10/js/index.js
+++ lesson10/js/index.js
@@ -47,39 +47,65 @@ const apiURL =
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
-
     document.querySelector("#description").innerHTML =
       jsObject.weather[0].description;
-    document.querySelector("#temp").innerHTML = jsObject.main.temp;
+    document.querySelector("#temp").innerHTML = Math.round(jsObject.main.temp);
     document.querySelector("#humidity").innerHTML = jsObject.main.humidity;
-    document.querySelector("#speed").innerHTML = jsObject.wind.speed;
+    document.querySelector("#speed").innerHTML = Math.round(
+      jsObject.wind.speed
+    );
+    // Windchill
+    const t = parseInt(jsObject.main.temp);
+    const s = parseInt(jsObject.wind.speed);
+    const feelsLike = `${Math.floor(windChill(t, s))} &deg;F`;
 
+    if (t <= 50 && s > 3) {
+      document.querySelector("#feels").innerHTML = feelsLike;
+    }
+
+    function windChill(temp, speed) {
+      let f =
+        35.74 +
+        0.6215 * temp -
+        35.75 * Math.pow(speed, 0.16) +
+        0.4275 * temp * Math.pow(speed, 0.16);
+      return f;
+    }
+  });
+
+// Forecast
+
+const fapiURL =
+  "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=bb328ca5ed25ab2e54b32f4fee76885b&units=imperial";
+
+fetch(fapiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    let day = 0;
+    const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    const forecast = jsObject.list.filter((forecast) =>
+      forecast.dt_txt.includes("12:00:00")
+    );
+
+    forecast.forEach((i) => {
+      let d = new Date(i.dt_txt);
+      document.getElementById(`dayofweek${day + 1}`).textContent =
+        dayOfWeek[d.getDay()];
+      document.getElementById(`forecast${day + 1}`).textContent = Math.round(
+        i.main.temp_max
+      );
+      day++;
+    });
     // const imagesrc =
-    //   "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png"; // note the concatenation
-    // const desc = jsObject.weather[0].description; // note how we reference the weather array
+    //   "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png";
     // document.getElementById("imagesrc").textContent = imagesrc; // informational specification only
     // document.getElementById("icon").setAttribute("src", imagesrc); // focus on the setAttribute() method
     // document.getElementById("icon").setAttribute("alt", desc);
+    // });
   });
-
-// Windchill
-const t = parseInt(document.querySelector("#temp").textContent);
-const s = parseInt(document.querySelector("#speed").textContent);
-const feelsLike = `${Math.floor(windChill(t, s))} &deg;F`;
-
-if (t <= 50 && s > 3) {
-  document.querySelector("#feels").innerHTML = feelsLike;
-}
-
-function windChill(temp, speed) {
-  let f =
-    35.74 +
-    0.6215 * temp -
-    35.75 * Math.pow(speed, 0.16) +
-    0.4275 * temp * Math.pow(speed, 0.16);
-  return f;
-}
 
 // Web font load
 WebFont.load({

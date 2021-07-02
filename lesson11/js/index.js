821708b17b9509299f357055873b5f708cfdf237
JulioCaballero63 <juliocl_14@hotmail.com>
2021-07-01 20:21:50 -0500

    update

--- lesson11/js/index.js
+++ lesson11/js/index.js
@@ -165,3 +165,37 @@ WebFont.load({
 function adjustRating(rating) {
   document.getElementById("ratingvalue").innerHTML = rating;
 }
+
+// Lazy load
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imageThreshold = {
+  threshold: 0.5,
+  rootMargin: "0px 0px 40px 0px",
+};
+
+const changeImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        changeImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imageThreshold);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    changeImages(img);
+  });
+}
